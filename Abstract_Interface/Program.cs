8581524306f39b2431e6ddb4aa15e2348382348f
andb70 <andb70@gmail.com>
2019-04-09 13:32:12 +0200

    Add project files.

---
+++ Abstract_Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abstract_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
